fab8c0a31609c89ff85da07b130a514fa3dc32a8
jonathanmao <44730418+jonathanmao@users.noreply.github.com>
2018-11-08 13:11:31 -0400

    Update hash.h

--- hash.h
+++ hash.h
@@ -1,135 +1,121 @@
-#include <pthread.h>
 
-#define TAMBUCKET 1024
+#include <pthread.h>
+#include<stdlib.h>
+#include<iostream>
+ using namespace std;
+ #define TAMBUCKET 1024
 #define BUCKETSIZE 256
 #define fhash(i) (i*2654435761) % TAMBUCKET
 
-class meuhash{
+ typedef struct OpenAdressingHash {
+	pthread_mutex_t lock;
+} hash_oa;
+
+ class meuhash{
 	public:
-		meuhash()
-		{
-			for(int i=0; i<64;i++)
-				mutex[i] = PHTHEAD_MUTEX_INITIALIZER;
-		}
-		int get(int chave);
-		void put(int chave);
+		meuhash(){
+			int i,a;
+	 		for(i = 0; i < TAMBUCKET; i++){
+				hashTable[i] = NULL;
+				//nao eh garantia que todo ponteiro inicia com NULL, por isso precisamos fazer isso
+			}
+	 		for(i = 0; i < 64; i++){
+				mutex_OA[i] = new hash_oa;
+				a = pthread_mutex_init(&(mutex_OA[i]->lock), NULL);
+				//cout<<i<<" "<<a<<endl;
+			}
+	 	}
+	 	int get(int chave);
+	 	void put(int chave);
 	private:
-		pthread_mutex_t mutex[64];
-		int hashTable[TAMBUCKET];
-		int tamanho;
+ 		int* hashTable[TAMBUCKET];
+		hash_oa *mutex_OA[64];
 };
 
-int meuhash::get(int chave)
-{
-	if(chave == NULL)
-			return NULL;
-		int hash  = fhash(chave);
-		int i = 0;
-		int mfind = hash/16;
-		while(hashTable[hash] != NULL && hashTable[hash] != chave && i<TAMBUCKET)
-		{
-			i++;
-			hash = (hash+1) % TAMBUCKET;
-			mfind = hash/16;
-		}
-		pthread_mutex_lock(&mutex[mfind]);
-		int ret = hashTable[hash];
-		if(ret != NULL){
-			hashTable[hash] = NULL;
-			tamanho --;
-		}
-		pthread_mutex_unlock(&mutex[mfind]);
-		return ret;
-
-}
 
 void meuhash::put(int chave)
 {
-	int hash = fhash(chave); 
-	int mfind = hash/16;
-	int i = 0;
-	//se ele estiver cheio procura o proximo espaço vazio
-	while(hashTable[hash] != NULL && hashTable[hash] != chave && i<TAMBUCKET)
-	{
-		i++;
-		hash = (hash+1) % TAMBUCKET;
-		mfind = hash/16;
-	}
-	pthread_mutex_lock(&mutex[mfind]);
-	hashTable[hash] = chave;
-	tamanho++;
-	pthread_mutex_unlock(&mutex[mfind]);
-}
-
-
-typedef struct hinode
-{
-	hinode *prox;
-	pthread_mutex_t mutex = PHTHEAD_MUTEX_INITIALIZER;
-	int content;
-}no;
-
-
-class meuhashEncadeado
-{
-	public:
-		meuhashEncadeado()
-		{}
-		void put(int chave);
-		int get(int chave);
-		int search(int chave);
-	private:
-		no *hashTable[BUCKETSIZE];
-		int tamanho;
-};
-
-
-void meuhashEncadeado::put(int chave)
-{
-	int hash = fhash(chave);
-	no *topo = hashTable[hash];
-	//se ele não estiver vazio insere depois do topo, e passa o fim da lista para ele
-	if(topo->content != NULL)
-	{
-		no *aux = new no;
-		pthread_mutex_lock(&aux->mutex);
-		aux->content = chave;
-		aux->prox = topo->prox;
-		topo->prox = aux;
-		pthread_mutex_unlock(&aux->mutex);
-
-	}else{
-		pthread_mutex_lock(&topo->mutex);
-		topo->content = chave;
-		pthread_mutex_unlock(&topo->mutex);
-	}
-}
-
-int meuhashEncadeado::get(int chave)
-{
-	int ret = -1;
-	int hash = fhash(chave);
-	no *aux = hashTable[hash];
-	//se ele não estiver vazio procura o fim da lista encadeada
-	if(aux->content != chave)
-	{
-		while(aux->prox!= NULL && aux->prox->content != chave)
-		{
-			aux = aux->prox;
+	// OpenAdressingMutex **hash_oa;
+	int positionHash,i,cont;
+	positionHash = fhash(chave);
+	cont = 0;
+	i = positionHash/16;
+	pthread_mutex_lock(&mutex_OA[i]->lock);
+	while( (hashTable[positionHash] != NULL) && cont < TAMBUCKET){
+ 		if(*hashTable[positionHash] == chave){
+			pthread_mutex_unlock(&mutex_OA[i]->lock);
+ 			return; //se a chave for repetida, nao insere
 		}
-		if (aux->prox == NULL)
-			return ret;
-		//apaga o proximo da lista
-		pthread_mutex_lock(&aux->mutex);
-		ret = aux->prox->content;
-		aux->prox = NULL;
-		pthread_mutex_unlock(&aux->mutex);
-		return ret;
+		pthread_mutex_unlock(&mutex_OA[i]->lock);
+		positionHash = (positionHash + 1) % TAMBUCKET;
+		cont++;
+		i = positionHash/16;
+		pthread_mutex_lock(&mutex_OA[i]->lock);
 	}
-	//apaga o topo da lista
-	pthread_mutex_lock(&aux->mutex);
-	ret = aux->content;
-	aux = NULL;
-	pthread_mutex_unlock(&aux->mutex);
-	return ret;
+	hashTable[positionHash] = new int;
+	*hashTable[positionHash] = chave;
+	//cout<<hashTable[positionHash]<<" "<<*hashTable[positionHash]<<endl;
+	pthread_mutex_unlock(&mutex_OA[i]->lock);
 }
+
+ // class meuhashEncadeado
+// {
+// 	public:
+// 		meuhashEncadeado()
+// 		{}
+// 		void put(int chave);
+// 		int get(int chave);
+// 		int search(int chave);
+// 	private:
+// 		no *hashTable[BUCKETSIZE];
+// 		int tamanho;
+// };
+//
+//
+// void meuhashEncadeado::put(int chave){
+// 	int hash = fhash(chave);
+// 	no *topo = hashTable[hash];
+// 	//se ele não estiver vazio insere depois do topo, e passa o fim da lista para ele
+// 	if(topo->content != NULL){
+// 		no *aux = new no;
+// 		pthread_mutex_lock(&aux->mutex);
+// 		aux->content = chave;
+// 		aux->prox = topo->prox;
+// 		topo->prox = aux;
+// 		pthread_mutex_unlock(&aux->mutex);
+//
+// 	}else{
+// 		pthread_mutex_lock(&topo->mutex);
+// 		topo->content = chave;
+// 		pthread_mutex_unlock(&topo->mutex);
+// 	}
+// }
+//
+// int meuhashEncadeado::get(int chave)
+// {
+// 	int ret = -1;
+// 	int hash = fhash(chave);
+// 	no *aux = hashTable[hash];
+// 	//se ele não estiver vazio procura o fim da lista encadeada
+// 	if(aux->content != chave)
+// 	{
+// 		while(aux->prox!= NULL && aux->prox->content != chave)
+// 		{
+// 			aux = aux->prox;
+// 		}
+// 		if (aux->prox == NULL)
+// 			return ret;
+// 		//apaga o proximo da lista
+// 		pthread_mutex_lock(&aux->mutex);
+// 		ret = aux->prox->content;
+// 		aux->prox = NULL;
+// 		pthread_mutex_unlock(&aux->mutex);
+// 		return ret;
+// 	}
+// 	//apaga o topo da lista
+// 	pthread_mutex_lock(&aux->mutex);
+// 	ret = aux->content;
+// 	aux = NULL;
+// 	pthread_mutex_unlock(&aux->mutex);
+// 	return ret;
+// }
